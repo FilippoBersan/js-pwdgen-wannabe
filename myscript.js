2a4b27c9d92c35f101200b366d8678603558c0d6
FilippoBersan <filippobersan99@gmail.com>
2023-10-31 14:49:01 +0100

    feat: javascript nome cognome colore

--- myscript.js
+++ myscript.js
@@ -7,3 +7,14 @@ const elementColore = document.getElementById('colore');
 elementNome.innerHTML = 'Il tuo nome: ';
 elementCognome.innerHTML = 'Il tuo cognome: ';
 elementColore.innerHTML = 'Il tuo colore preferito: ';
+
+// const nomeInput = prompt('Inserisci il tuo nome:');
+// const cognomeInput = prompt('Inserisci il tuo cognome:');
+// const coloreInput = prompt('Inserisci il tuo colore preferito:');
+
+elementNome.innerHTML =
+  elementNome.innerHTML + prompt('Inserisci il tuo nome:');
+elementCognome.innerHTML =
+  elementCognome.innerHTML + prompt('Inserisci il tuo cognome:');
+elementColore.innerHTML =
+  elementColore.innerHTML + prompt('Inserisci il tuo colore preferito:');
