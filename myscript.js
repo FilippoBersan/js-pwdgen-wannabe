58906114fe1b72e01f653a60ec1783153efa9932
FilippoBersan <filippobersan99@gmail.com>
2023-10-31 14:26:59 +0100

    feat: scaffolding e struttura

---
+++ myscript.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const elementNome = document.getElementById('nome');
+const elementCognome = document.getElementById('cognome');
+const elementColore = document.getElementById('colore');
+
+elementNome.innerHTML = 'Il tuo nome: ';
+elementCognome.innerHTML = 'Il tuo cognome: ';
+elementColore.innerHTML = 'Il tuo colore preferito: ';
